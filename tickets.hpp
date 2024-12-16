bc98fd2cf51f02dd69c5bb2f98ae5f375498c3e4
Pokoyokjk <mgcumia@Pokoyo.>
2024-12-16 18:45:34 +0100

    initial commit

---
+++ tickets.hpp
@@ -0,0 +1,16 @@
+// superclass for ticket management system of a concert
+
+#ifndef TICKETS_HPP_INCLUDED
+
+#define TICKETS_HPP_INCLUDED
+
+#include <iostream>
+
+class Tickets
+{
+public:
+    Tickets();
+
+};
+
+#endif // TICKETS_HPP_INCLUDED
