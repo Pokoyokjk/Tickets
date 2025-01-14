17abd716721df29184227e448cec44e62527b896
Pokoyokjk <133882385+Pokoyokjk@users.noreply.github.com>
2025-01-14 01:26:28 +0100

    Update tickets.hpp

--- tickets.hpp
+++ tickets.hpp
@@ -1,15 +1,38 @@
 // superclass for ticket management system of a concert
 
 #ifndef TICKETS_HPP_INCLUDED
-
 #define TICKETS_HPP_INCLUDED
 
+#include <string>
 #include <iostream>
 
-class Tickets
-{
+using namespace std;
+
+class Tickets{
+protected:
+    int ticketID;
+    string ticketType;
+    string ticketPrice;
+    string ticketStatus;
+    string ticketColor;
 public:
-    Tickets();
+    Tickets(int ticketID, string ticketType, string ticketPrice, string ticketStatus, string ticketColor);
+    
+    void setTicketID(int ticketID);
+    void setTicketType(string ticketType);
+    void setTicketPrice(string ticketPrice);
+    void setTicketStatus(string ticketStatus);
+    void setTicketColor(string ticketColor);
+
+    int getTicketID() const;
+    string getTicketType() const;
+    string getTicketPrice() const;
+    string getTicketStatus() const;
+    string getTicketColor() const;
+
+    void print() const;
+
+    
 
 };
 
