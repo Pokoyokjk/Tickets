77eeb7e88f08ddc8bd589b43cd9f9860dcca67d5
Pokoyokjk <133882385+Pokoyokjk@users.noreply.github.com>
2025-01-14 01:28:01 +0100

    Add files via upload

---
+++ tickets.cpp
@@ -0,0 +1,52 @@
+#include "Tickets.hpp"
+
+// this will be used to create a ticket object
+Tickets::Tickets(int ticketID, string ticketType, string ticketPrice, string ticketStatus, string ticketColor) : ticketID(ticketID), ticketType(ticketType), ticketPrice(ticketPrice), ticketStatus(ticketStatus), ticketColor(ticketColor) {
+}
+
+void Tickets::setTicketID(int ticketID){
+    this->ticketID = ticketID;
+}
+
+void Tickets::setTicketType(string ticketType){
+    this->ticketType = ticketType;
+}
+
+void Tickets::setTicketPrice(string ticketPrice){
+    this->ticketPrice = ticketPrice;
+}
+
+void Tickets::setTicketStatus(string ticketStatus){
+    this->ticketStatus = ticketStatus;
+}
+
+void Tickets::setTicketColor(string ticketColor){
+    this->ticketColor = ticketColor;
+}
+
+int Tickets::getTicketID() const{
+    return this->ticketID;
+}
+
+string Tickets::getTicketType() const{
+    return this->ticketType;
+}
+
+string Tickets::getTicketPrice() const{
+    return this->ticketPrice;
+}
+
+string Tickets::getTicketStatus() const{
+    return this->ticketStatus;
+}
+
+string Tickets::getTicketColor() const{
+    return this->ticketColor;
+}
+
+void Tickets::print() const{
+    cout << "Problem! Cannot describe ticket :(" << endl;
+}
+
+
+
