4e235583b173ef72a646f2106e76ab4124c10124
Pokoyokjk <133882385+Pokoyokjk@users.noreply.github.com>
2025-01-16 13:41:06 +0100

    main

--- main.cpp
+++ main.cpp
@@ -12,5 +12,6 @@ int main()  {
     cout << "-----------------" << endl;
     // (int ticketID, string concertGroup, int ticketPrice, string timeHour, string ticketColor, string ticketStatus, int seat, string person, bool withSeat)
     GeneralTicket gt1(00001, "Extremoduro", 100, "20:00, 12/12/2021", "Red", "Available", 1, "Gloria", true);
-    //cout << gt1.toString() << endl;
+    cout << "The name of the band is: " << gt1.getConcertGroup() << endl;
+    cout << "The ticket includes a seat: " << gt1.getWithSeat() << endl;
 }
