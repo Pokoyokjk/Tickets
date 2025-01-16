ddd48e585960f8af38e28a114cb9beb26de7fe7c
Pokoyokjk <133882385+Pokoyokjk@users.noreply.github.com>
2025-01-16 13:18:28 +0100

    error

--- main.cpp
+++ main.cpp
@@ -12,5 +12,5 @@ int main()  {
     cout << "-----------------" << endl;
     // (int ticketID, string concertGroup, int ticketPrice, string timeHour, string ticketColor, string ticketStatus, int seat, string person, bool withSeat)
     GeneralTicket gt1(00001, "Extremoduro", 100, "20:00, 12/12/2021", "Red", "Available", 1, "Gloria", true);
-    cout << gt1.toString() << endl;
+    //cout << gt1.toString() << endl;
 }
