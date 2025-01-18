40f7878d8ebdac9e88b244e7c409bacad48ccfb7
Pokoyokjk <133882385+Pokoyokjk@users.noreply.github.com>
2025-01-18 13:58:14 +0100

    main

--- main.cpp
+++ main.cpp
@@ -9,19 +9,28 @@ using namespace std;
 
 int main()  {
 
+    //creating an array of ticket IDs
+    int i = 0;
+
     cout << "-----------------" << endl;
     // (int ticketID, string concertGroup, int ticketPrice, string timeHour, string ticketColor, string ticketStatus, int seat, string person, bool withSeat)
-    GeneralTicket gt1(00001, "Extremoduro", 100, "20:00, 12/12/2021", "Red", "Available", 1, "Gloria", true);
+    //for a General ticket, me setting the values
+    GeneralTicket gt1(0, "Extremoduro", 100, "20:00, 12/12/2021", "Red", "Available", 1, "Gloria", true);
+    gt1.setTicketID(i);
+    // Changing the ticket ID for the next ticket
+    i++;
+
     cout << "The name of the band is: " << gt1.getConcertGroup() << endl;
     cout << "The ticket includes a seat: " << gt1.getWithSeat() << endl;
+    gt1.printTicket();
+
     
-    //now lets create another General icket in which we ask in the command line to set the values
+    //now lets create another General ticket in which we ask in the command line to set the values
     cout << "-----------------" << endl;
     GeneralTicket gt2 (0, "", 0, "", "", "", 0, "", false);
-    int ticketID;
-    cout << "Please enter the ticket ID: ";
-    cin >> ticketID;
-    gt2.setTicketID(ticketID);
+    
+    gt2.setTicketID(i);
+    i++;
 
     string concertGroup;
     cout << "Please enter the concert group: ";
@@ -34,7 +43,7 @@ int main()  {
     gt2.setTicketPrice(ticketPrice);
 
     string timeHour;
-    cout << "Please enter the time of the concert: ";
+    cout << "Please enter the time and day of the concert: ";
     getline(cin >> ws, timeHour);
     gt2.setTimeHour(timeHour);
 
@@ -44,7 +53,7 @@ int main()  {
     gt2.setTicketColor(ticketColor);
 
     string ticketStatus;
-    cout << "Please enter the ticket status: ";
+    cout << "Please enter the ticket status (available, not available): ";
     cin >> ticketStatus;
     gt2.setTicketStatus(ticketStatus);
 
@@ -57,6 +66,30 @@ int main()  {
     cout << "Please enter if the ticket includes a seat (1 for yes, 0 for no): ";
     cin >> withSeat;
     gt2.setWithSeat(withSeat);
+
+    if (gt2.getWithSeat() == 1) {
+        int seat;
+        cout << "Please enter the seat number: ";
+        cin >> seat;
+        gt2.setSeat(seat);
+    }
+
     gt2.printTicket();
 
 }
+
+// Let's create a VIP ticket
+    cout << "-----------------" << endl;
+    // (int ticketID, string concertGroup, int ticketPrice, string timeHour, string ticketColor, string ticketStatus, 
+    // int seat, string person, bool backStagePass, bool meetAndGreet, bool foodAndDrink)
+    //for a VIP ticket, me setting the values
+    VIPTicket vt1(00222, "Extremoduro", 200, "20:00, 12/12/2021", "Pink", "Available", 1, "Rodrigo", true, true, true);
+    cout << "The name purchaser's name is: " << vt1.getPerson() << endl;
+
+    //now lets create another VIP ticket in which we ask in the command line to set some values
+    cout << "-----------------" << endl;
+    VIPTicket vt2 (0, "", 0, "", "", "", 0, "", false, false, false);
+    int ticketID;
+    cout << "Please enter the ticket ID: ";
+    cin >> ticketID;
+    vt2.setTicketID(ticketID);
--- tickets.cpp
+++ tickets.cpp
@@ -5,6 +5,7 @@ Tickets::Tickets(int ticketID, string concertGroup, int ticketPrice, string time
     ticketID(ticketID), concertGroup(concertGroup), ticketPrice(ticketPrice), timeHour(timeHour), ticketColor(ticketColor), ticketStatus(ticketStatus), seat(seat), person(person) {
     }
 
+// this method will just give a ticket ID to the ticket, it will be like a couter. The first ID=1, the second ID=2, and so on.
 void Tickets::setTicketID(int ticketID) {
     this->ticketID = ticketID;
 }
